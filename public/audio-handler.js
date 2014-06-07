bd9c0d2f1878d25a0a0cd43216da2ea2e4a3a1fb
cohen990 <dancohen447@gmail.com>
2014-06-07 18:29:47 +0100

    audiohandler.js

---
+++ public/audio-handler.js
@@ -0,0 +1,22 @@
+function soundBites(color){
+    if(color=="red"){
+        var soundBite = document.getElementById("creaking");
+        var modifier = 0.98;
+    }
+    else if(color=="green"){
+        var soundBite = document.getElementById("wind");
+        var modifier = 0;
+    }
+    else if(color=="yellow"){
+        var soundBite = document.getElementById("rain");
+        var modifier = 0;
+    }
+    else
+    {
+        return null;
+    }
+
+    if(Math.random() > modifier){
+        soundBite.play();
+    }
+}
